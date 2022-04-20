95775f82024b9a426cd760dd6c04c025cce455ce
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2022-04-20 16:27:43 +0100

    feat: colors

--- src/theme/system/colors.ts
+++ src/theme/system/colors.ts
@@ -1,7 +1,18 @@
 const colors = {
-  background: "#121214",
-  text: "#e1e1e6",
-  primary: "#8257e6",
+  text: "#404040",
+
+  primary: "#3f3f3f",
+
+  white: "#ffffff",
+
+  menu: "#e1efd8",
+
+  // ppages colors
+  home: "#ddeaf6",
+  about: "#fff2cc",
+  articles: "#d5dbe5",
+  works: "#d8e2f3",
+  skills: "#ededed"
 };
 
 export default colors;
