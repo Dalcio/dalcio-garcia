b4e8084afdb43cbd35ceb5742a72479431e82ab3
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2022-04-28 16:13:51 +0100

    feat: menu items animation

--- src/components/Menu/content/index.tsx
+++ src/components/Menu/content/index.tsx
@@ -1,50 +1,7 @@
-import { MenuContentContainer } from './styles';
-import { MenuContentProps, MenuItemProps } from './types';
+import { MenuContentContainer, MenuNav } from './styles';
+import { MenuContentProps } from './types';
 
-import Link from 'next/link';
-import styled from 'styled-components';
-
-import { RouteColorName } from '@theme/system/colors.types';
-
-const ItemLink = styled.a<{ color: RouteColorName }>`
-	cursor: pointer;
-	font-size: 40px;
-
-	&:hover {
-		display: inline-block;
-		font-size: 42px;
-		border-botom: 2px solid ${({ color, theme }) => theme.colors[color]};
-	}
-
-	&.is-active {
-		& > span:first-of-type {
-			display: inline-block;
-			background: ${({ color, theme }) => theme.colors.primary};
-			color: ${({ color, theme }) => theme.colors[color]};
-		}
-	}
-`;
-
-export const MenuItem = ({ name, current }: MenuItemProps) => (
-	<li>
-		<Link href={`/${name === 'home' ? '' : name}`}>
-			<ItemLink
-				color={current}
-				className={current === name ? 'is-active' : undefined}
-			>
-				<span>{name.substr(0, 2)}</span>
-				<span>{name.substr(2)}</span>
-			</ItemLink>
-		</Link>
-	</li>
-);
-
-export const MenuNav = styled.ul`
-	margin-top: ${({ theme }) => `calc(2 * ${theme.space.md})`};
-	list-style: none;
-	displya: grid;
-	row-gap: ${({ theme }) => theme.space.sm};
-`;
+import MenuItem from './MenuItem';
 
 const MenuContent = ({ current }: MenuContentProps) => {
 	return (
@@ -55,7 +12,6 @@ const MenuContent = ({ current }: MenuContentProps) => {
 				<MenuItem name="works" current={current} />
 				<MenuItem name="articles" current={current} />
 			</MenuNav>
-			<h1>The content of menu just goes here</h1>
 		</MenuContentContainer>
 	);
 };
--- src/components/Menu/content/styles.ts
+++ src/components/Menu/content/styles.ts
@@ -5,4 +5,17 @@ export const MenuContentContainer = styled.main`
 	height: 100%;
 	padding: ${({ theme }) => theme.space.md};
 	padding-top: calc(3 * ${({ theme }) => theme.space.md});
+	overflow: auto;
+`;
+
+export const MenuNav = styled.ul`
+	list-style: none;
+	display: grid;
+	row-gap: ${({ theme }) => theme.space.sm};
+	margin-top: ${({ theme }) => `calc(2 * ${theme.space.md})`};
+	padding-left: ${({ theme }) => theme.space.md};
+
+	li {
+		position: relative;
+	}
 `;
